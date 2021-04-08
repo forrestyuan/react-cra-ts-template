049d2e4e0d606ad2329e769862ff081483ef415e
forrestyuan <2316539148@qq.com>
2021-04-08 18:58:45 +0800

    init proj

--- src/modules/login/store.ts
+++ src/modules/login/store.ts
@@ -1,5 +1,5 @@
 import { makeAutoObservable } from 'mobx'
-import { createStore } from '../../common/utils/create-store'
+import { createStore } from '@/common/utils/create-store'
 class LoginStore {
   constructor() {
     makeAutoObservable(this)
